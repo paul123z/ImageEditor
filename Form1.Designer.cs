b5c47bb5a87545f03d47949271f31efbc678b9a7
Pavol Lantaj <palolantaj@gmail.com>
2022-07-24 13:39:18 +0200

    browse image,save image,reload, first filter ALL WORKING

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -68,8 +68,9 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(178, 46);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "Reload";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -77,8 +78,9 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(178, 46);
             this.button2.TabIndex = 2;
-            this.button2.Text = "button2";
+            this.button2.Text = "Filter 2";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
--- Form1.cs
+++ Form1.cs
@@ -57,6 +57,70 @@ namespace ImageEditor
         }
 
 
+
+
+
+        void filter2()
+        {
+            if (!opened)
+            {
+                MessageBox.Show("Open an Image first, then apply changes");
+            }
+            else
+            {
+
+                Image img = pictureBox1.Image;                             // storing image into img variable of image type from picturebox1
+                Bitmap bmpInverted = new Bitmap(img.Width, img.Height);   /* creating a bitmap of the height of imported picture in picturebox which consists of the pixel data for a graphics image
+                                                                        and its attributes. A Bitmap is an object used to work with images defined by pixel data.*/
+
+                ImageAttributes ia = new ImageAttributes();                 //creating an object of imageattribute ia to change the attribute of images
+                ColorMatrix cmPicture = new ColorMatrix(new float[][]       // now creating the color matrix object to change the colors or apply  image filter on image
+                {
+            new float[]{.393f, .349f+0.5f, .272f, 0, 0}, //red
+            new float[]{.769f+0.3f, .686f, .534f, 0, 0}, //green
+            new float[]{.189f, .168f, .131f+0.5f, 0, 0}, //blue
+            new float[]{0, 0, 0, 1, 0}, //hue
+            new float[]{0, 0, 0, 0, 1} //saturation
+                });
+                ia.SetColorMatrix(cmPicture);           //pass the color matrix to imageattribute object ia
+                Graphics g = Graphics.FromImage(bmpInverted);   /*create a new object of graphics named g, ; Create graphics object for alteration.
+                                                            Graphics newGraphics = Graphics.FromImage(imageFile); is the format of loading image into graphics for alteration*/
+
+                g.DrawImage(img, new Rectangle(0, 0, img.Width, img.Height), 0, 0, img.Width, img.Height, GraphicsUnit.Pixel, ia);
+
+
+                /*   g.drawimage(image, new rectangle(location of rectangle axix-x, location axis-y, width of rectangle, height of rectangle),
+               location of image in rectangle x-axis, location of image in rectangle y-axis, width of image, height of image,
+               format of graphics unit,provide the image attributes   */
+
+
+                g.Dispose();                            //Releases all resources used by this Graphics.
+                pictureBox1.Image = bmpInverted;
+
+            }
+        }
+
+
+
+
+        void reload()
+        {
+            if (!opened)
+            {
+                //MessageBox.Show("Open an Image then apply changes");
+            }
+            else 
+            {
+                if (opened)
+                {
+                    file = Image.FromFile(openFileDialog1.FileName);
+                    pictureBox1.Image = file;
+                    opened = true;
+                }
+            }
+        }
+
+
         Image file;
         Boolean opened = false; //to check whether image is open in picture box or not
 
@@ -73,6 +137,16 @@ namespace ImageEditor
         private void button8_Click(object sender, EventArgs e)
         {
             saveImage();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            filter2();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            reload();
         }
     }
 }
